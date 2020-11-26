fbf8a7ba87f5f85c0ec46883ddcc2eab53914251
David Linner <david@linner.berlin>
2020-11-26 08:41:04 +0100

    Added a website with an example of the fetch API.

--- main.js
+++ main.js
@@ -1,9 +1,35 @@
 const express = require('express');
-const app = express();
 
-app.get('/', (request, response) => {
-    response.json({status:'ok'});
+const app = express();
+app.use(express.json());
+app.use(express.static('public'));
+
+const items = {};
+
+app.get('/items', (request, response) => {
+    response.json(items);
 });
+
+app.put('/items/:id([0-9]+)', (request, response) => {
+    const item = request.body;
+    const {id} = request.params;
+    items[id] = item;
+    response.send();
+});
+
+app.post('/items', (request, response) => {
+    const item = request.body;
+
+    const highestId = Object
+        .keys(items)
+        .map(parseInt)
+        .reduce((max, current) => current > max ? current : max, 0);
+
+    items[highestId + 1] = item;
+    response.send();
+});
+
+
 app.listen(4000, function () {
     console.log('Example app listening on port 4000!');
 })
