69e507134b2111e18a8199a0497801d74445b3ac
David Linner <david.linner@smarthometeam.de>
2021-11-10 12:01:34 +0100

    Added a route /ingredients to enumerate ingredients of a recipe.

--- src/main.js
+++ src/main.js
@@ -4,8 +4,35 @@ const app = express();
 
 const PORT = process.env.PORT || 4000
 
-app.use(cors());
+const INGREDIENTS = [
+    {
+        title: 'Flour',
+        quantity: 100,
+        unit: 'g'
+    },
+    {
+        title: 'Egg',
+        quantity: 2,
+        unit: 'pieces'
+    },
+    {
+        title: 'Milk',
+        quantity: 300,
+        unit: 'ml'
+    },
+    {
+        title: 'Sugar',
+        quantity: 1,
+        unit: 'tbsp'
+    },
+    {
+        title: 'Oil for frying',
+        quantity: 1,
+        unit: 'tbsp'
+    }
+];
 
+app.use(cors());
 
 app.get('/', (request, response) => {
     response.json({status:'ok'});
@@ -22,6 +49,14 @@ app.get('/html', (request, response) => {
         .send( '<h1>hello fetch! Do you recognize html?</h1>');
 });
 
+app.get('/ingredients/:i', (request, response) => {
+    const {i} = request.params;
+    if(i >= 0 && i < INGREDIENTS.length) {
+        response.json(INGREDIENTS[i]);
+    } else {
+        response.status(404).end();
+    }
+});
 
 app.listen(PORT, function () {
     console.log(`Example API server app listening on port ${PORT}!`);
